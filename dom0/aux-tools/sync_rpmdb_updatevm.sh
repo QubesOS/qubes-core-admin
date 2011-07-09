87ebdeefd4618a92a7d3608d8573a0faed452c01
Marek Marczykowski <marmarek@mimuw.edu.pl>
2011-07-09 21:12:11 +0200

    dom0: use qrexec_client instead of qvm-run to not start guid
    guid doesn't makes sense in /etc/init.d/qubes_netvm

--- dom0/aux-tools/sync_rpmdb_updatevm.sh
+++ dom0/aux-tools/sync_rpmdb_updatevm.sh
@@ -3,7 +3,7 @@
 UPDATEVM=`qvm-get-updatevm`
 
 if [ -n "$UPDATEVM" ]; then
-    qvm-run -u root --pass_io --localcmd='tar c /var/lib/rpm /etc/yum.repos.d' "$UPDATEVM" 'tar x -C /var/lib/qubes/dom0-updates'
+    /usr/lib/qubes/qrexec_client -d "$UPDATEVM" -l 'tar c /var/lib/rpm /etc/yum.repos.d' 'root:tar x -C /var/lib/qubes/dom0-updates'
 fi
 
 # Ignore errors (eg VM not running)
