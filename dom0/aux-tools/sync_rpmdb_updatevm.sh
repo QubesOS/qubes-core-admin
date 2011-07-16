02ae961bf0f908df9582aaa9c363f110d159f4cb
Marek Marczykowski <marmarek@mimuw.edu.pl>
2011-07-16 02:01:31 +0200

    dom0: hide warnings from tar (#265)
    Unfortunately tar in Fedora 13 is to old to support --warning option, which
    disables only particular kind of warnings..

--- dom0/aux-tools/sync_rpmdb_updatevm.sh
+++ dom0/aux-tools/sync_rpmdb_updatevm.sh
@@ -3,7 +3,7 @@
 UPDATEVM=`qvm-get-updatevm`
 
 if [ -n "$UPDATEVM" ]; then
-    /usr/lib/qubes/qrexec_client -d "$UPDATEVM" -l 'tar c /var/lib/rpm /etc/yum.repos.d' 'root:tar x -C /var/lib/qubes/dom0-updates'
+    /usr/lib/qubes/qrexec_client -d "$UPDATEVM" -l 'tar c /var/lib/rpm /etc/yum.repos.d 2>/dev/null' 'user:tar x -C /var/lib/qubes/dom0-updates'
 fi
 
 # Ignore errors (eg VM not running)
