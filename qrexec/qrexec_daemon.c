1b42142e05cee257a968178d7a4b7636a9166016
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-10-18 00:09:34 +0200

    dom0/qrexec: change qrexec startup timeout to 60s (#373)

--- qrexec/qrexec_daemon.c
+++ qrexec/qrexec_daemon.c
@@ -82,7 +82,7 @@ int create_qrexec_socket(int domid, char *domname)
 	return get_server_socket(socket_address);
 }
 
-#define MAX_STARTUP_TIME 30
+#define MAX_STARTUP_TIME 60
 
 /* do the preparatory tasks, needed before entering the main event loop */
 void init(int xid)
