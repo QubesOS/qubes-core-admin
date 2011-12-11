d84d64716bbf273895a140b368f399ed6cb191fa
Marek Marczykowski <marmarek@invisiblethingslab.com>
2011-12-11 14:09:46 +0100

    dom0: start guid only when shm.id present
    Eg. not during firstboot

--- dom0/qvm-core/qubes.py
+++ dom0/qvm-core/qubes.py
@@ -1145,7 +1145,7 @@ class QubesVm(object):
                 self.force_shutdown()
                 raise OSError ("ERROR: Cannot execute qrexec_daemon!")
 
-        if subprocess.call('xprop -root >/dev/null 2>&1', shell=True) == 0:
+        if os.path.exists('/var/run/shm.id'):
             self.start_guid(verbose=verbose)
 
         if preparing_dvm:
