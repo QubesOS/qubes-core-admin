8689368a112cc48eb2f8067c3f70a64efff9a628
Marek Marczykowski <marmarek@invisiblethingslab.com>
2012-02-06 19:07:13 +0100

    vm/mimeopen: merge user defaults with system one (#423)

--- vm-systemd/misc-post-stop.sh
+++ vm-systemd/misc-post-stop.sh
@@ -3,6 +3,9 @@
 # Save default applications for DispVM
 
 su -c 'mkdir -p /home_volatile/user/.local/share/applications' user
-su -c 'cp -a /home/user/.local/share/applications/defaults.list /home_volatile/user/.local/share/applications/' user
+su -c 'cp -a /usr/share/applications/defaults.list /home_volatile/user/.local/share/applications/' user
+if [ -r '/home/user/.local/share/applications/defaults.list' ]; then
+    su -c 'cat /home/user/.local/share/applications/defaults.list >> /home_volatile/user/.local/share/applications/defaults.list' user
+fi
 
 exit 0
