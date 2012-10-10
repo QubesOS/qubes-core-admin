331c27d34425c7f7ff09017ecd86876ae66800d6
Alexandre Bezroutchko <abb@gremwell.com>
2012-10-10 22:59:27 +0200

    dvp/qvm-usb: fix backend install script to ignore some errors

--- install-pvusb-backend.sh
+++ install-pvusb-backend.sh
@@ -12,9 +12,9 @@ cp misc/usb_remove /usr/lib/qubes/usb_remove
 cp misc/qubes_usb.rules /etc/udev/rules.d/99-qubes_usb.rules
 
 # Reload PVUSB backend and cleanup xenstore
-rmmod xen-usbback || true
+rmmod xen-usbback > /dev/null 2>&1 || true
 modprobe xen-usbback
-xenstore-rm qubes-usb-devices
+xenstore-rm qubes-usb-devices > /dev/null 2>&1 || true
 
 # Configure udevd and make it re-populate xenstore
 udevadm control --reload-rules
