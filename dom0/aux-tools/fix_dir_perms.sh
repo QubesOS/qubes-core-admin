4f71c5aeda649c0a65d80b0a3bf73c8a930ed7d7
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-01-25 03:13:01 +0100

    dom0/init: create /var/run/qubes at startup and set permissions
    If /var/run is on tmpfs (which is the case in fc18), its subdirs must be
    recreated at each reboot. As we already have script for similar things, just
    extend it.
    Also make /proc/xen/xenbus user-accessible for xenstore reads/writes.

--- dom0/aux-tools/fix_dir_perms.sh
+++ dom0/aux-tools/fix_dir_perms.sh
@@ -9,8 +9,13 @@ chgrp qubes /var/log/xen
 chmod 770 /var/log/xen
 chgrp qubes /proc/xen/privcmd
 chmod 660 /proc/xen/privcmd
+chgrp qubes /proc/xen/xenbus
+chmod 660 /proc/xen/xenbus
 chgrp qubes /dev/xen/evtchn
 chmod 660 /dev/xen/evtchn
+mkdir -p /var/run/qubes
+chgrp qubes /var/run/qubes
+chmod 770 /var/run/qubes
 touch /var/run/qubes/xl-lock
 chgrp qubes /var/run/qubes/xl-lock
 chmod 660 /var/run/qubes/xl-lock
