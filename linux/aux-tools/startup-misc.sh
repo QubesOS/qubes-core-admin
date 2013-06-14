7484ddfd2b0952dc9d2409dadd2dd9c81cc73206
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2013-06-14 05:28:08 +0200

    startup-misc: populate local domain id
    Apparently libvirt doesn't do it.

--- linux/aux-tools/startup-misc.sh
+++ linux/aux-tools/startup-misc.sh
@@ -4,6 +4,7 @@
 
 /usr/lib/qubes/fix-dir-perms.sh
 xenstore-write /local/domain/0/name dom0
+xenstore-write domid 0
 DOM0_MAXMEM=`/usr/sbin/xl info | grep total_memory | awk '{ print $3 }'`
 xenstore-write /local/domain/0/memory/static-max $[ $DOM0_MAXMEM * 1024 ]
 
