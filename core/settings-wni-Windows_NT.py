e2bea656b4c8223cf60cf2238aab397b077acee9
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2013-08-02 20:29:30 +0200

    Add vm-configs for WNI

--- core/settings-wni-Windows_NT.py
+++ core/settings-wni-Windows_NT.py
@@ -6,5 +6,7 @@ from qubes.storage.wni import QubesWniVmStorage
 
 def apply(system_path, vm_files, defaults):
     system_path['qubes_base_dir'] = 'c:\\qubes'
+    system_path['config_template_pv'] = 'c:/program files/Invisible Things Lab/Qubes/vm-template.xml'
+    system_path['config_template_hvm'] = 'c:/program files/Invisible Things Lab/Qubes/vm-template-hvm.xml'
     defaults['libvirt_uri'] = 'test:///default'
     defaults['storage_class'] = QubesWniVmStorage
