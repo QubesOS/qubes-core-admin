6a0a932fbd0dfd4b0c85e7a8f1354ec664aeda1f
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-12-12 23:09:33 +0100

    core: fix vm.run(..., passio=False) handling
    Long time ago passio=True was used to replace current process with
    qrexec-client directly (qvm-run --pass-io was the called), but this
    behaviour is not used anymore (qvm-run was the only user). And this
    option was left untouched, with misleading name - one would assume that
    using passio=False should disallow any I/O, but this isn't the case.
    
    Especially qvm-sync-clock is calling clockvm.run('...', wait=True),
    default value for passio=False. This causes to output data from
    untrusted VM, without sanitising terminal sequences, which can be fatal.
    
    This patch changes passio semantic to actually do what it means - when
    set to True - VM process will be able to interact with
    stdin/stdout/stderr. But when set to False, all those FDs will be
    connected to /dev/null.

--- core-modules/000QubesVm.py
+++ core-modules/000QubesVm.py
@@ -1553,14 +1553,14 @@ class QubesVm(object):
             args += ["-t"]
         if os.isatty(sys.stderr.fileno()):
             args += ["-T"]
-        if passio:
-            os.execv(system_path["qrexec_client_path"], args)
-            exit(1)
 
         call_kwargs = {}
-        if ignore_stderr:
-            null = open("/dev/null", "w")
+        if ignore_stderr or not passio:
+            null = open("/dev/null", "rw")
             call_kwargs['stderr'] = null
+        if not passio:
+            call_kwargs['stdin'] = null
+            call_kwargs['stdout'] = null
 
         if passio_popen:
             popen_kwargs={'stdout': subprocess.PIPE}
@@ -1573,7 +1573,7 @@ class QubesVm(object):
             if null:
                 null.close()
             return p
-        if not wait:
+        if not wait and not passio:
             args += ["-e"]
         retcode = subprocess.call(args, **call_kwargs)
         if null:
