12092c3aa5cb3144c8c862329d8cfff7ef72cca9
Wojtek Porczyk <woju@invisiblethingslab.com>
2015-06-30 17:06:35 +0200

    core3: port TemplateVM

--- qubes/vm/templatevm.py
+++ qubes/vm/templatevm.py
@@ -7,8 +7,34 @@ import qubes.vm.qubesvm
 class TemplateVM(qubes.vm.qubesvm.QubesVM):
     '''Template for AppVM'''
 
-    def __init__(self, D):
-        super(TemplateVM, self).__init__(D)
+    @property
+    def rootcow_img(self):
+        '''COW image'''
+        return self.storage.rootcow_img
+
+
+    def __init__(self, *args, **kwargs):
+        super(TemplateVM, self).__init__(*args, **kwargs)
 
         # Some additional checks for template based VM
         assert self.root_img is not None, "Missing root_img for standalone VM!"
+
+
+    def clone_disk_files(self, src):
+        super(QubesTemplateVm, self).clone_disk_files(src)
+
+        # Create root-cow.img
+        self.commit_changes()
+
+
+    def commit_changes(self):
+        '''Commit changes to template'''
+        self.log.debug('commit_changes()')
+
+        if not self.app.vmm.offline_mode:
+            assert not self.is_running(), \
+                'Attempt to commit changes on running Template VM!'
+
+        self.log.info(
+            'Commiting template update; COW: {}'.format(self.rootcow_img))
+        self.storage.commit_template_changes()
