18edf4946c5b1ad6650c7c92bfda75bf3bd99096
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-10-10 03:39:43 +0200

    dispvm: get rid of obsolete symlinks to dvm.conf
    Separate config file is no longer created for DispVM - the configuration
    is passed directly to libvirt.
    
    Fixes QubesOS/qubes-issues#1314

--- dispvm/startup-dvm.sh
+++ dispvm/startup-dvm.sh
@@ -6,10 +6,8 @@ printf "\x00\x00\x00\x00" > /var/run/qubes/dispVM.seq
 chown root:qubes /var/run/qubes/dispVM.seq
 chmod 660 /var/run/qubes/dispVM.seq
 DEFAULT=/var/lib/qubes/dvmdata/default-savefile
-DEFAULT_CONFIG=/var/lib/qubes/dvmdata/default-dvm.conf
 # setup DispVM files only when they exists
 if [ -r $DEFAULT ]; then
-    ln -s $DEFAULT_CONFIG /var/run/qubes/current-dvm.conf
     if [ -f /var/lib/qubes/dvmdata/dont-use-shm ] ; then
 	ln -s $DEFAULT /var/run/qubes/current-savefile
     else
