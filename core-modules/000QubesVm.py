65255626e22a8c1031bc51c4dd714d98f0f42614
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-03-07 03:54:21 +0100

    core: add separate QubesDB entry for primary DNS address
    Fixes QubesOS/qubes-issues#1817

--- core-modules/000QubesVm.py
+++ core-modules/000QubesVm.py
@@ -1105,6 +1105,7 @@ class QubesVm(object):
 
         if self.is_netvm():
             self.qdb.write("/qubes-netvm-gateway", self.gateway)
+            self.qdb.write("/qubes-netvm-primary-dns", self.gateway)
             self.qdb.write("/qubes-netvm-secondary-dns", self.secondary_dns)
             self.qdb.write("/qubes-netvm-netmask", self.netmask)
             self.qdb.write("/qubes-netvm-network", self.network)
@@ -1113,6 +1114,7 @@ class QubesVm(object):
             self.qdb.write("/qubes-ip", self.ip)
             self.qdb.write("/qubes-netmask", self.netvm.netmask)
             self.qdb.write("/qubes-gateway", self.netvm.gateway)
+            self.qdb.write("/qubes-primary-dns", self.netvm.gateway)
             self.qdb.write("/qubes-secondary-dns", self.netvm.secondary_dns)
 
         tzname = self.get_timezone()
