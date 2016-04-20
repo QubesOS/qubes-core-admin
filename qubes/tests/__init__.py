a1575b98d4b7167f1c7b6848dc6bc4eefff26b49
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-04-20 02:25:06 +0200

    tests: do not drop 'qubes.tests.' prefix
    This cause troubles with "external" tests (loaded from outside of
    qubes.tests module).

--- qubes/tests/__init__.py
+++ qubes/tests/__init__.py
@@ -223,7 +223,7 @@ class QubesTestCase(unittest.TestCase):
 
     def __str__(self):
         return '{}/{}/{}'.format(
-            '.'.join(self.__class__.__module__.split('.')[2:]),
+            self.__class__.__module__,
             self.__class__.__name__,
             self._testMethodName)
 
