2164a8d7b8b3d788d42c65c3b092498d57304c41
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-10-12 00:11:50 +0200

    Change license to LGPL v2.1+
    See this thread for reasoning and acceptance from contributors:
    https://groups.google.com/d/topic/qubes-devel/G7KzrfU0lWY/discussion
    "Changing qubes-core-admin license to LGPL v2.1+"

--- qubes/backup.py
+++ qubes/backup.py
@@ -5,18 +5,18 @@
 #                                   <marmarek@invisiblethingslab.com>
 # Copyright (C) 2013  Olivier Médoc <o_medoc@yahoo.fr>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #
 #
 from __future__ import unicode_literals
