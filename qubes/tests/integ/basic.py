fd9f2e2a6c7360bab0391dc2b2ba58e3006cb6b8
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-10-15 05:17:19 +0200

    tests: type commands into specific found window
    Make sure events are sent to specific window found with xdotool search,
    not the one having the focus. In case of Whonix, it can be first
    connection wizard or whonixcheck report.

--- qubes/tests/integ/basic.py
+++ qubes/tests/integ/basic.py
@@ -108,7 +108,7 @@ class TC_00_Basic(qubes.tests.SystemTestCase):
         self.assertTrue(self.vm.is_running())
         # Type 'poweroff'
         subprocess.check_call(['xdotool', 'search', '--name', self.vm.name,
-                               'type', 'poweroff\r'])
+                               'type', '--window', '%1', 'poweroff\r'])
         for _ in range(10):
             if not self.vm.is_running():
                 break
@@ -674,7 +674,7 @@ class TC_06_AppVMMixin(object):
         self.assertTrue(self.vm.is_running())
         # Type 'poweroff'
         subprocess.check_call(['xdotool', 'search', '--name', self.vm.name,
-                               'type', 'poweroff\r'])
+                               'type', '--window', '%1', 'poweroff\r'])
         for _ in range(10):
             if not self.vm.is_running():
                 break
