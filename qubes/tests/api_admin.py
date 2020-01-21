bd293fc4d14fa10fde5137ba65749c04089720ee
Marta Marczykowska-Górecka <marta.maria.gorecka@gmail.com>
2020-01-21 22:16:54 +0100

    Added handling for 'force' option in vm shutdown
    To be more precise: just allowed using it, as the option was
    actually implemented previously.
    
    references QubesOS/qubes-issues#5591
    
    (cherry picked from commit 284fbe1d4477ed98c64bfae7b04a6aa96e050270)

--- qubes/tests/api_admin.py
+++ qubes/tests/api_admin.py
@@ -955,7 +955,18 @@ class TC_00_VMs(AdminAPITestCase):
         self.vm.shutdown = coroutine_mock
         value = self.call_mgmt_func(b'admin.vm.Shutdown', b'test-vm1')
         self.assertIsNone(value)
-        func_mock.assert_called_once_with()
+        func_mock.assert_called_once_with(force=False)
+
+    def test_231_shutdown_force(self):
+        func_mock = unittest.mock.Mock()
+
+        @asyncio.coroutine
+        def coroutine_mock(*args, **kwargs):
+            return func_mock(*args, **kwargs)
+        self.vm.shutdown = coroutine_mock
+        value = self.call_mgmt_func(b'admin.vm.Shutdown', b'test-vm1', b'force')
+        self.assertIsNone(value)
+        func_mock.assert_called_once_with(force=True)
 
     def test_240_pause(self):
         func_mock = unittest.mock.Mock()
@@ -2640,7 +2651,6 @@ class TC_00_VMs(AdminAPITestCase):
             b'admin.vm.firewall.Reload',
             b'admin.vm.volume.List',
             b'admin.vm.Start',
-            b'admin.vm.Shutdown',
             b'admin.vm.Pause',
             b'admin.vm.Unpause',
             b'admin.vm.Kill',
