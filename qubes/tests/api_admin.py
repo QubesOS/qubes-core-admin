311f8c39c8ee8c79a2dc2a1fba8c0440a22b5ec2
Rusty Bird <rustybird@net-c.com>
2020-06-22 14:03:16 +0000

    api/admin: report unexpected driver options in admin.pool.Add
    Fixes QubesOS/qubes-issues#5895
    
    (cherry picked from commit 8f1d59e9af96cf0cc839eb9aaf9603c16bb8a7ad)

--- qubes/tests/api_admin.py
+++ qubes/tests/api_admin.py
@@ -693,7 +693,7 @@ class TC_00_VMs(AdminAPITestCase):
 
         add_pool_mock, self.app.add_pool = self.coroutine_mock()
 
-        with self.assertRaises(qubes.api.PermissionDenied):
+        with self.assertRaises(qubes.exc.QubesException):
             self.call_mgmt_func(b'admin.pool.Add', b'dom0',
                 b'driver1', b'name=test-pool\nparam3=some-value\n')
         self.assertEqual(mock_drivers.mock_calls, [unittest.mock.call()])
