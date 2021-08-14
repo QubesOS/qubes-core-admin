6e3e250dae006a69639149b1212456f4c8b909bb
Giulio <giulio@gmx.com>
2021-08-14 15:05:07 +0200

    fix parsing error

--- qubes/vm/mix/net.py
+++ qubes/vm/mix/net.py
@@ -411,7 +411,7 @@ class NetVMMixin(qubes.events.Emitter):
                 if forwardtype == "internal":
                     base_dir = '/qubes-firewall-forward/{}/{}/'.format(vm.name, ip)
                     self.untrusted_qdb.write(base_dir + key, value)
-                    self.untrusted_qdb.write(base_dir + '/last', '1')
+                    self.untrusted_qdb.write(base_dir + 'last', '1')
                     self.untrusted_qdb.write(base_dir[:-1], '')
                 elif forwardtype == "external":
                     current_ip = ip
@@ -419,7 +419,7 @@ class NetVMMixin(qubes.events.Emitter):
                         base_dir = '/qubes-firewall-forward/{}/{}/'.format(vm.name, current_ip)
                         netvm.untrusted_qdb.write(base_dir + key, value)
                         if i == len(netpath)-1:
-                            netvm.untrusted_qdb.write(base_dir + '/last', '1')
+                            netvm.untrusted_qdb.write(base_dir + 'last', '1')
                         current_ip = netvm.ip
                         self.untrusted_qdb.write(base_dir[:-1], '')
                 else:
