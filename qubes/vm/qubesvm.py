f54cb6a71a799a98a51f26b34a4410965f4de03f
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-08-15 01:50:45 +0200

    Do not try to define libvirt domain if it isn't starting
    Libvirt domain object, even if defined, isn't guaranteed to be up to
    date, if the domain isn't running. Do not pretend it is. QubesVM.start()
    method explicitly define the domain in libvirt. When accessing
    QubesVM.libvirt_domain property, return None if it isn't defined already
    (instead of trying to define it) - as the docstring already says.
    
    The immediate reason is that accessing Volume.block_device() isn't
    guaranteed to work before Volume.start(). But this change also speedups
    getting -1 XID if VM is not running - it definitely isn't, if it isn't
    defined in libvirt and there is no sense in defining it just to confirm
    this.
    
    While at it, remove redundant checking for QubesVM.libvirt_domain being
    none - QubesVM.is_running() already handles this case. And update the
    tests to take this into account.

--- qubes/vm/qubesvm.py
+++ qubes/vm/qubesvm.py
@@ -761,8 +761,6 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
         Or not Xen, but ID.
         """
 
-        if self.libvirt_domain is None:
-            return -1
         try:
             if self.is_running():
                 return self.libvirt_domain.ID()
@@ -821,13 +819,12 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
         if self.app.vmm.offline_mode:
             return None
 
-        # XXX _update_libvirt_domain?
         try:
             self._libvirt_domain = self.app.vmm.libvirt_conn.lookupByUUID(
                 self.uuid.bytes)
         except libvirt.libvirtError as e:
             if e.get_error_code() == libvirt.VIR_ERR_NO_DOMAIN:
-                self._update_libvirt_domain()
+                return None
             else:
                 raise
         return self._libvirt_domain
