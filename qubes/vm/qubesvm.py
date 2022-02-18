QubesOS/qubes-core-admin

commit 8463b6e0ce7725de107470fddada7b2b2d7198be

_kill_locked(): Wait for VIR_DOMAIN_EVENT_STOPPED
This ensures that the domain is actually dead, and that a spurious
stopped event will not happen.  Furthermore, it ensures that the storage
has been released, even if the libvirt API call returns immediately
rather than waiting for the VM to actually be destroyed.

Prior to encrypting volatile volumes, this bug did not actually cause
problems.  For the file and reflink pools, the underlying loop device is
clenaed up by the block script, and the file on disk is deleted when the
last reference to it goes away.  The LVM2 pool, on the other hand, calls
‘blkdiscard’ before removing the storage.  ‘blkdiscard’ does _not_
require the block device to be unused to have stopped, and is slow on
thin pools due to a Linux kernel bug.  Therefore, by the time LVM2 is
actually called to removed the volume, the VM will likely have actually
stopped, meaning that the volume will no longer be in use.

However, ‘cryptsetup close’ is called _before_ ‘blkdiscard’, and it
_does_ require the volume to be unused.  This makes the problem much
more likely to show up.  ‘cryptsetup close’ has built-in retry logic, so
if the VM stops soon enough, the only symptom will be a ‘device-mapper:
remove ioctl on <device name> failed: device or resource busy’ warning
printed to stderr.  However, if the VM takes too long to stop,
‘cryptsetup close’ will fail outright, preventing the storage from
stopping.

Furthermore, if ‘VIR_DOMAIN_EVENT_STOPPED’ is delivered at exactly the
wrong time, it is possible for the storage to be stopped _twice_.  This
results in bad behavior, such as LVM2 failing with ENOENT.

(cherry picked from commit 8d5fa6d18e5fe29ec868cd2e728aa96556598a0e)

qubes/vm/qubesvm.py
@@ -765,6 +765,7 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
                 node_hvm.getparent().remove(node_hvm)
 
         super().__init__(app, xml, **kwargs)
+        self.__waiter = None
 
         if volume_config is None:
             volume_config = {}
@@ -1151,6 +1152,10 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
             # or subsequent start()
             return
 
+        if self.__waiter is not None:
+            self.__waiter.set_result(None)
+            self.__waiter = None
+
         self._domain_stopped_event_received = True
         self._domain_stopped_future = \
             asyncio.ensure_future(self._domain_stopped_coro())
@@ -1237,9 +1242,10 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
 
     @asyncio.coroutine
     def _kill_locked(self):
-        '''Forcefully shutdown (destroy) domain.
+        """Forcefully shutdown (destroy) domain.
 
-        This function needs to be called with self.startup_lock held.'''
+        This function needs to be called with self.startup_lock held."""
+        self.__waiter = asyncio.get_running_loop().create_future()
         try:
             self.libvirt_domain.destroy()
         except libvirt.libvirtError as e:
@@ -1248,6 +1254,10 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
             raise
 
         # make sure all shutdown tasks are completed
+        try:
+            yield from asyncio.wait_for(self.__waiter, timeout=10)
+        except asyncio.TimeoutError:
+            pass
         yield from self._ensure_shutdown_handled()
 
     @asyncio.coroutine

commit 9415b321cf97b019cc376072b01c27b3a15b3fc6

vm/qubesvm: fix killing VMs
Use get_event_loop() to be compatible with Python 3.5.4 in R4.0 dom0.
Killing VMs was broken after 8463b6e (a cherry-pick), e.g.:

unhandled exception while calling src=b'dom0' meth=b'admin.vm.Kill' dest=b'disp1234' arg=b'' len(untrusted_payload)=0
Traceback (most recent call last):
  File "/usr/lib/python3.5/site-packages/qubes/api/__init__.py", line 275, in respond
    untrusted_payload=untrusted_payload)
  File "/usr/lib64/python3.5/asyncio/futures.py", line 381, in __iter__
    yield self  # This tells Task to wait for completion.
  File "/usr/lib64/python3.5/asyncio/tasks.py", line 310, in _wakeup
    future.result()
  File "/usr/lib64/python3.5/asyncio/futures.py", line 294, in result
    raise self._exception
  File "/usr/lib64/python3.5/asyncio/tasks.py", line 240, in _step
    result = coro.send(None)
  File "/usr/lib/python3.5/site-packages/qubes/api/admin.py", line 864, in vm_kill
    yield from self.dest.kill()
  File "/usr/lib/python3.5/site-packages/qubes/vm/qubesvm.py", line 1239, in kill
    yield from self._kill_locked()
  File "/usr/lib/python3.5/site-packages/qubes/vm/qubesvm.py", line 1248, in _kill_locked
    self.__waiter = asyncio.get_running_loop().create_future()
AttributeError: module 'asyncio' has no attribute 'get_running_loop'

qubes/vm/qubesvm.py
@@ -1245,7 +1245,7 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
         """Forcefully shutdown (destroy) domain.
 
         This function needs to be called with self.startup_lock held."""
-        self.__waiter = asyncio.get_running_loop().create_future()
+        self.__waiter = asyncio.get_event_loop().create_future()
         try:
             self.libvirt_domain.destroy()
         except libvirt.libvirtError as e: