76c0328842c92d4ba3cec7c6d4006010ad674185
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-05-01 04:05:02 -0400

    Better error message on invalid property values
    This results in the exception being reported to the Admin API caller,
    which is what is desired here.  Previously the user got an less helpful
    error message.

--- qubes/vm/__init__.py
+++ qubes/vm/__init__.py
@@ -475,9 +475,7 @@ class VMProperty(qubes.property):
             if self.allow_none:
                 super().__set__(instance, value)
                 return
-            raise ValueError(
-                'Property {!r} does not allow setting to {!r}'.format(
-                    self.__name__, value))
+            raise qubes.exc.QubesPropertyValueError(instance, self, value)
 
         app = instance if isinstance(instance, qubes.Qubes) else instance.app
 
