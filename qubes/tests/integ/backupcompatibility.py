522145b461de3ed368af3f89847ad340188a9458
Rusty Bird <rustybird@net-c.com>
2022-11-04 14:12:49 +0000

    tests/integ/backupcompatibility: hardcode MD5 algo for format v3
    QubesOS/qubes-issues#7852
    
    (cherry picked from commit fdbf1d792795b5914f2e0089451f685f990b55b1)

--- qubes/tests/integ/backupcompatibility.py
+++ qubes/tests/integ/backupcompatibility.py
@@ -292,7 +292,8 @@ class TC_00_BackupCompatibility(
         tar = subprocess.Popen(tar_cmdline, stdout=subprocess.PIPE)
         if encrypted:
             encryptor = subprocess.Popen(
-                ["openssl", "enc", "-e", "-aes-256-cbc", "-pass", "pass:qubes"],
+                ["openssl", "enc", "-e", "-aes-256-cbc",
+                 "-md", "MD5", "-pass", "pass:qubes"],
                 stdin=tar.stdout,
                 stdout=subprocess.PIPE)
             data = encryptor.stdout
