d3f44ae32be01900166c3dedbce35b835ad36a12
Piotr Bartman-Szwarc <prbartman@invisiblethingslab.com>
2024-08-20 00:24:03 +0200

    q-dev: deny list

--- qubes/ext/admin.py
+++ qubes/ext/admin.py
@@ -16,6 +16,8 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, see <https://www.gnu.org/licenses/>.
+import importlib
+import sys
 
 import qubes.api
 import qubes.api.internal
@@ -23,6 +25,9 @@ import qubes.exc
 import qubes.ext
 import qubes.vm.adminvm
 from qrexec.policy import utils, parser
+
+from qubes.device_protocol import DeviceInterface
+from qubes.devices import DEVICE_DENY_LIST
 
 
 class JustEvaluateAskResolution(parser.AskResolution):
@@ -160,3 +165,37 @@ class AdminExtension(qubes.ext.Extension):
         tag_with = created_by.features.get('tag-created-vm-with', '')
         for tag_with_single in tag_with.split():
             vm.tags.add(tag_with_single)
+
+    @qubes.ext.handler(*(f'admin-permission:admin.vm.device.{ep.name}.Attach'
+        for ep in importlib.metadata.entry_points(group='qubes.devices')))
+    def on_usb_device_attacg(
+            self, vm, event, dest, arg, device, mode, options, **kwargs
+    ):
+        # ignore auto-attachment
+        if mode != 'manual':
+            return
+
+        # load device deny list
+        deny = {}
+        try:
+            with open(DEVICE_DENY_LIST, 'r') as file:
+                for line in file:
+                    line = line.strip()
+
+                    if line:
+                        name, *values = line.split()
+
+                        values = ' '.join(values).replace(',', ' ').split()
+                        values = set([v for v in values if len(v) > 0])
+
+                        deny[name] = deny.get(name, set()).union(set(values))
+        except IOError:
+            pass
+
+        # check if any presented interface is on deny list
+        for interface in deny.get(dest.name, set()):
+            pattern = DeviceInterface(interface)
+            for devint in device.interfaces:
+                if pattern.matches(devint):
+                    raise qubes.exc.PermissionDenied()
+
