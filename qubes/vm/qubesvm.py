QubesOS/qubes-core-admin

commit ba8bc65520b36899043d63a115ad43efdf83fe74

Allow setting "none" kernel to use in-vm kernel in any virt_mode
Automatically translate it to appropriate pvgrub flavor.

QubesOS/qubes-issues#5212

qubes/vm/qubesvm.py
@@ -1645,6 +1645,18 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
     @property
     def kernel_path(self):
         if not self.kernel:
+            if self.virt_mode == 'pvh':
+                return os.path.join(
+                    qubes.config.qubes_base_dir,
+                    qubes.config.system_path['qubes_kernels_base_dir'],
+                    "pvgrub2-pvh",
+                    "vmlinuz")
+            if self.virt_mode == "pv":
+                return os.path.join(
+                    qubes.config.qubes_base_dir,
+                    qubes.config.system_path['qubes_kernels_base_dir'],
+                    "pvgrub2",
+                    "vmlinuz")
             return None
         return self.storage.kernels_dir + "/vmlinuz"
 
@@ -1663,9 +1675,9 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
         if self.virt_mode == 'hvm':
             return not self.kernel
         if self.virt_mode == 'pvh':
-            return self.kernel == 'pvgrub2-pvh'
+            return not self.kernel or self.kernel == 'pvgrub2-pvh'
         if self.virt_mode == 'pv':
-            return self.kernel == 'pvgrub2'
+            return not self.kernel or self.kernel == 'pvgrub2'
         assert False
 
     @property

qubes/tests/vm/qubesvm.py
@@ -504,7 +504,7 @@ class TC_90_QubesVM(QubesVMTestsMixin, qubes.tests.QubesTestCase):
 
     @unittest.mock.patch('qubes.config.qubes_base_dir', '/tmp/qubes-test')
     def test_250_kernel(self):
-        for kver in ("dummy", "dummy2"):
+        for kver in ("dummy", "dummy2", "pvgrub2", "pvgrub2-pvh"):
             kernel_dir = '/tmp/qubes-test/vm-kernels/' + kver
             os.makedirs(kernel_dir, exist_ok=True)
             open(os.path.join(kernel_dir, 'vmlinuz'), 'w').close()
@@ -517,6 +517,12 @@ class TC_90_QubesVM(QubesVMTestsMixin, qubes.tests.QubesTestCase):
             "dummy2")
         del vm.kernel
         self.assertPropertyDefaultValue(vm, 'kernel', "dummy")
+        vm.kernel = None
+        self.assertEqual(vm.kernel_path, "/tmp/qubes-test/vm-kernels/pvgrub2-pvh/vmlinuz")
+        vm.virt_mode = "pv"
+        self.assertEqual(vm.kernel_path, "/tmp/qubes-test/vm-kernels/pvgrub2/vmlinuz")
+        vm.virt_mode = "hvm"
+        self.assertIsNone(vm.kernel_path)
 
     @qubes.tests.skipUnlessDom0
     def test_251_kernel_invalid(self):

commit 9498dfe5cdd8cea5b09b62cae27ebdee77ab5efd

Remove too strict checking for kernel on startup
Nowadays empty kernel is automatically converted to pvgrub2, so don't
refuse that value.

Fixes: ba8bc655 "Allow setting "none" kernel to use in-vm kernel in any virt_mode"

QubesOS/qubes-issues#5212

qubes/vm/qubesvm.py
@@ -1188,9 +1188,6 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
                                 f'not available'
                             )
 
-                if self.virt_mode == 'pvh' and not self.kernel:
-                    raise qubes.exc.QubesException(
-                        'virt_mode PVH require kernel to be set')
                 await self.storage.verify()
 
                 if self.netvm is not None: