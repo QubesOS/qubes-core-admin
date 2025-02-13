4a98ef398a138dc4def3289f95d2f0d0ed11b9fe
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-02-12 23:57:36 -0600

    (wip) Add boot mode support

--- qubes/ext/core_features.py
+++ qubes/ext/core_features.py
@@ -24,6 +24,8 @@ import string
 import qubes.ext
 
 _version_re = re.compile(r"[0-9]{1,3}\.[0-9]{1,3}")
+_bootmode_name_key_re = re.compile(r"boot-mode\.[-a-z_]*\.name$")
+_bootmode_kernelopts_key_re = re.compile(r"boot-mode\.[-a-z_]*\.kernelopts$")
 
 
 class CoreFeatures(qubes.ext.Extension):
@@ -104,6 +106,34 @@ class CoreFeatures(qubes.ext.Extension):
             untrusted_value = untrusted_features["qubes-agent-version"]
             if _version_re.fullmatch(untrusted_value):
                 vm.features["qubes-agent-version"] = untrusted_value
+
+        # handle boot mode advertisement
+        for _, (
+            untrusted_feature_key, untrusted_feature_value
+        ) in enumerate(untrusted_features.items()):
+            if not all(
+                c in string.printable for c in untrusted_feature_value
+            ):
+                continue
+            if _bootmode_name_key_re.match(untrusted_feature_key):
+                bootmode_feature = untrusted_feature_key
+                bootmode_value = untrusted_feature_value
+                vm.features[bootmode_feature] = bootmode_value
+            elif _bootmode_kernelopts_key_re.match(untrusted_feature_key):
+                # Don't allow setting the kernelopts for the default mode,
+                # these are ignored anyway
+                if untrusted_feature_key == \
+                    "boot-mode.default.kernelopts":
+                    continue
+                bootmode_feature = untrusted_feature_key
+                bootmode_value = untrusted_feature_value
+                vm.features[bootmode_feature] = bootmode_value
+            elif untrusted_feature_key == "boot-mode.active" or \
+                untrusted_feature_key == "boot-mode.appvm-default":
+                bootmode_feature = untrusted_feature_key
+                bootmode_value = untrusted_feature_value
+                if bootmode_feature not in vm.features:
+                    vm.features[bootmode_feature] = bootmode_value
         del untrusted_features
 
         # default user for qvm-run etc
