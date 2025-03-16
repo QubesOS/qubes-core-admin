055d56f216268ce8e9453798a55f98fa12095b53
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2025-03-16 15:36:06 +0100

    tests: fix invalid device BDF
    PCI device function is just one hex digit.
    
    (cherry picked from commit c02059889ae8de3b0f4c7b943520ab47b561a18a)

--- qubes/tests/vm/init.py
+++ qubes/tests/vm/init.py
@@ -76,7 +76,7 @@ class TC_10_BaseVM(qubes.tests.QubesTestCase):
             </features>
 
             <devices class="pci">
-                <device backend-domain="domain1" id="00_11.22">
+                <device backend-domain="domain1" id="00_11.2">
                   <option name="no-strict-reset">True</option>
                 </device>
             </devices>
@@ -111,7 +111,7 @@ class TC_10_BaseVM(qubes.tests.QubesTestCase):
 
         self.assertCountEqual(vm.devices.keys(), ('pci',))
         self.assertCountEqual(list(vm.devices['pci'].persistent()),
-            [qubes.ext.pci.PCIDevice(vm, '00_11.22')])
+            [qubes.ext.pci.PCIDevice(vm, '00_11.2')])
 
         assignments = list(vm.devices['pci'].assignments())
         self.assertEqual(len(assignments), 1)
