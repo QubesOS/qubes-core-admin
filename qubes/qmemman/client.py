0abfd9ed1c48197bc74a6c7e2cba1a1182e6cd2e
Ben Grande <ben.grande.b@gmail.com>
2025-07-04 12:50:33 +0200

    Fix pylint complaints
    - Enable pylint;
    - Rename keyword 'id' or variable 'i' to 'domid', which is always xid
      but better to use a VMM agnostic name;
    - Move constants to module scope;
    - Pylint trips on dictionary iteration of .keys() with
      'consider-using-dict-items' and 'unnecessary-dict-index'. Fixing that
      helped find some useless assignments which were already objects of the
      dictionary;
    - Assign current domain dictionary domid looping to an object for easier
      reading and modification; and
    - Separate 'do_balance()' to avoid 'too-many-nested-blocks'.

--- qubes/qmemman/client.py
+++ qubes/qmemman/client.py
@@ -1,5 +1,3 @@
-# pylint: skip-file
-
 #
 # The Qubes OS Project, http://www.qubes-os.org
 #
@@ -24,20 +22,18 @@ import fcntl
 
 
 class QMemmanClient:
+    def __init__(self):
+        self.sock = None
+
     def request_memory(self, amount):
         self.sock = socket.socket(socket.AF_UNIX)
-
         flags = fcntl.fcntl(self.sock.fileno(), fcntl.F_GETFD)
         flags |= fcntl.FD_CLOEXEC
         fcntl.fcntl(self.sock.fileno(), fcntl.F_SETFD, flags)
-
         self.sock.connect("/var/run/qubes/qmemman.sock")
         self.sock.send(str(int(amount)).encode("ascii") + b"\n")
         received = self.sock.recv(1024).strip()
-        if received == b"OK":
-            return True
-        else:
-            return False
+        return bool(received == b"OK")
 
     def close(self):
         self.sock.close()
