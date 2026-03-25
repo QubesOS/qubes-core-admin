QubesOS/qubes-core-admin

commit 1ae6bae552ca7973f48645f0502a12d158684c4e

lvm: suppress logs on stdout when expecting json output
Apparently lvm sometimes logs messages to stdout (instead of stderr),
which messes up json format. Disable those messages by passing --quiet
option.

Fixes QubesOS/qubes-issues#10023

qubes/storage/lvm.py
@@ -234,6 +234,7 @@ class ThinPool(qubes.storage.Pool):
 _init_cache_cmd = [
     _lvm,
     "lvs",
+    "--quiet",
     "--noheadings",
     "-o",
     "vg_name,pool_lv,name,lv_size,data_percent,lv_attr,origin,lv_metadata_size,"

commit 8389b666251c9e50c7c2f1a494a700be773b3759

lvm: really suppress logs on stdout
Warnings on stdout are suppressed in silent mode, not just quiet. Use
--quiet twice to enable silent mode. See referenced commit for more
details.

Fixes: 1ae6bae5 "lvm: suppress logs on stdout when expecting json output"
Fixes QubesOS/qubes-issues#10023

qubes/storage/lvm.py
@@ -235,6 +235,7 @@ _init_cache_cmd = [
     _lvm,
     "lvs",
     "--quiet",
+    "--quiet",
     "--noheadings",
     "-o",
     "vg_name,pool_lv,name,lv_size,data_percent,lv_attr,origin,lv_metadata_size,"