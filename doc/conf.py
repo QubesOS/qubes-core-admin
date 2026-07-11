dba1b84cbdf5b8980ed4374fdfb8a301fa72dde7
parulin <161326115+parulin@users.noreply.github.com>
2026-07-11 19:41:17 +0000

    Link to other qubes docs with intersphinx
    Fix issues with references to glossary terms
    
    (cherry picked from commit 92d8945698c5ab292b6714ad473f5fc9dd16877e)

--- doc/conf.py
+++ doc/conf.py
@@ -279,4 +279,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/', None)}
+    'python': ('http://docs.python.org/', None),
+    'qubes-doc': ('https://doc.qubes-os.org/en/latest/', None),
+    'core-admin-client': ('https://dev.qubes-os.org/projects/core-admin-client/en/latest/', None),
+    'core-qrexec': ('https://dev.qubes-os.org/projects/qubes-core-qrexec/en/stable/', None),
+}
