QubesOS/qubes-core-admin

commit aa714dd8fd29d57bab54c4284193787f53bf92a3

Fix devices and other behavior for remotevms
- make remotevms not 'include in backups' by default
- make devices not crap out on remotevms

qubes/api/admin.py
@@ -1818,7 +1818,10 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
     async def vm_device_available(self, endpoint):
         devclass = endpoint
         try:
-            devices = self.dest.devices[devclass].get_exposed_devices()
+            if self.dest.klass == "RemoteVM":
+                devices = []
+            else:
+                devices = self.dest.devices[devclass].get_exposed_devices()
         except AttributeError as e:
             if e.name == "devices":
                 # shutdown in progress, return specific error
@@ -1854,9 +1857,12 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
     async def vm_device_list(self, endpoint):
         devclass = endpoint
         try:
-            device_assignments = list(
-                self.dest.devices[devclass].get_assigned_devices()
-            )
+            if self.dest.klass == "RemoteVM":
+                device_assignments = []
+            else:
+                device_assignments = list(
+                    self.dest.devices[devclass].get_assigned_devices()
+                )
         except AttributeError as e:
             if e.name == "devices":
                 # shutdown in progress, return specific error

qubes/ext/utils.py
@@ -82,6 +82,8 @@ def device_list_change(
 
     to_attach: Dict[str, Dict] = {}
     for front_vm in vm.app.domains:
+        if front_vm.klass == "RemoteVM":
+            continue
         if not front_vm.is_running():
             continue
         for assignment in reversed(

qubes/features.py
@@ -161,7 +161,7 @@ class Features(dict):
             raise NotImplementedError("app does not have features yet")
 
         assert isinstance(
-            self.subject, _vm.LocalVM
+            self.subject, _vm.BaseVM
         ), "recursive checks do not work for {}".format(
             type(self.subject).__name__
         )

qubes/vm/remotevm.py
@@ -24,6 +24,12 @@ from qubes.vm import BaseVM
 
 
 class RemoteVM(BaseVM):
+    include_in_backups = qubes.property(
+        "include_in_backups",
+        type=bool,
+        default=False,
+        doc="If this domain is to be included in default backup.",
+    )
 
     relayvm = qubes.VMProperty(
         "relayvm",

commit 9fa3b63ea7c12eebc75518efe649527fb47b2a56

api/admin: do not crash on accessing devices for RemoteVM
RemoteVM doesn't have devices. When listing devices (either exposed or
attached) simply return an empty list (this was already mostly done, but
one place was missing). But when trying to attach/detach, return an
error early.

Fixes: aa714dd8 "Fix devices and other behavior for remotevms"
Fixes QubesOS/qubes-issues#10915

qubes/api/admin.py
@@ -1912,9 +1912,12 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
     async def vm_device_attached(self, endpoint):
         devclass = endpoint
         try:
-            device_assignments = self.dest.devices[
-                devclass
-            ].get_attached_devices()
+            if self.dest.klass == "RemoteVM":
+                device_assignments = []
+            else:
+                device_assignments = self.dest.devices[
+                    devclass
+                ].get_attached_devices()
         except AttributeError as e:
             if e.name == "devices":
                 # shutdown in progress, return specific error
@@ -1984,6 +1987,9 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
             options=assignment.options,
         )
 
+        if not hasattr(self.dest, "devices"):
+            raise qubes.exc.QubesException("This qube doesn't support devices")
+
         await self.dest.devices[devclass].assign(assignment)
         self.app.save()
 
@@ -2026,6 +2032,9 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
 
         self.fire_event_for_permission(device=dev)
 
+        if not hasattr(self.dest, "devices"):
+            raise qubes.exc.QubesException("This qube doesn't support devices")
+
         await self.dest.devices[devclass].unassign(assignment)
         self.app.save()
 
@@ -2054,6 +2063,9 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
             device=dev, mode=assignment.mode.value, options=assignment.options
         )
 
+        if not hasattr(self.dest, "devices"):
+            raise qubes.exc.QubesException("This qube doesn't support devices")
+
         await self.dest.devices[devclass].attach(assignment)
 
     # Attach/Detach action can modify only a volatile state of running VM.
@@ -2075,6 +2087,9 @@ class QubesAdminAPI(qubes.api.AbstractQubesAPI):
         dev = self.load_device_info(devclass)
 
         self.fire_event_for_permission(device=dev)
+
+        if not hasattr(self.dest, "devices"):
+            raise qubes.exc.QubesException("This qube doesn't support devices")
 
         assignment = qubes.device_protocol.DeviceAssignment(dev)
         await self.dest.devices[devclass].detach(assignment)