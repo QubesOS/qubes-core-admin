QubesOS/qubes-core-admin

commit 6f23c689b53f25a8f137e952e70a67d5621d2596

Fix loading ephemeral_volatile pool property from qubes.xml
Similarly to ce22ff42 "Fix bug in qubesvm.py incorrectly treating
"False" config values as True", properly saving False values caused also
interpreting loaded "False" string as True bool. Fix this by adding a
helper function for deserializing bool properties.

For now, it's used only for ephemeral_volatile, as it's the only bool
property of a pool. Volumes and VMs already have their own handling of
bool properties.

qubes/utils.py
@@ -159,6 +159,17 @@ def size_to_human(size):
     return str(round(size / (1024.0 * 1024 * 1024), 1)) + " GiB"
 
 
+def parse_bool(value):
+    """Deserialize bool property, handling usual encodings"""
+    if isinstance(value, bool):
+        return value
+    if value in ("True", "1", "on"):
+        return True
+    if value in ("False", "0", "off", ""):
+        return False
+    raise ValueError(f"invalid bool: {value}")
+
+
 def urandom(size):
     rand = os.urandom(size)
     if rand is None:

qubes/storage/__init__.py
@@ -1005,7 +1005,7 @@ class Pool:
         self._volumes_collection = VolumesCollection(self)
         self.name = name
         self.revisions_to_keep = revisions_to_keep
-        self.ephemeral_volatile = ephemeral_volatile
+        self.ephemeral_volatile = qubes.utils.parse_bool(ephemeral_volatile)
 
     def __eq__(self, other):
         if isinstance(other, Pool):

commit 158d01a2fb0787f444e2af70dccf78d5f3ead228

Fix https://github.com/QubesOS/qubes-issues/issues/11045     qubes.property.bool lowers string so it accepts true/false, while qubes.utils.parse_bool only accepts capitalized True/False, this fix modifed parse_bool to accept "true" and "false"
    This was breaking qubesd and was caused in https://github.com/QubesOS/qubes-core-admin/commit/6f23c689b53f25a8f137e952e70a67d5621d2596

qubes/utils.py
@@ -163,9 +163,9 @@ def parse_bool(value):
     """Deserialize bool property, handling usual encodings"""
     if isinstance(value, bool):
         return value
-    if value in ("True", "1", "on"):
+    if value in ("True", "true", "1", "on"):
         return True
-    if value in ("False", "0", "off", ""):
+    if value in ("False", "false", "0", "off", ""):
         return False
     raise ValueError(f"invalid bool: {value}")
 