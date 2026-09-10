d9fb66705a6cf74e109f4aa51332d93e44761624
Ben Grande <ben@invisiblethingslab.com>
2026-09-10 10:15:16 -0400

    Skip membalance on paused domain
    I did not face the xen-balloon high CPU usage, but this commit prevents
    paused domains from being donors or acceptors, which should avoid other
    domains from trying to free memory or acquire memory due to unused
    quantity.
    
    The domain is balanced again after unpause and the next qmemmam cycle.
    
    Fixes: https://github.com/QubesOS/qubes-issues/issues/11121

--- qubes/qmemman/systemstate.py
+++ qubes/qmemman/systemstate.py
@@ -127,6 +127,8 @@ class SystemState:
                 continue
             dom = self.dom_dict[domid]
             dom.paused = paused
+            if dom.paused:
+                dom.no_progress = True
             # Real memory usage
             dom.mem_current = domain["mem_kb"] * 1024
             # What VM is using or can use
@@ -156,7 +158,7 @@ class SystemState:
     def clear_outdated_error_markers(self) -> None:
         # Clear outdated errors.
         for dom in self.dom_dict.values():
-            if dom.mem_used is None:
+            if dom.mem_used is None or dom.paused:
                 continue
             # Clear markers excluding VM from memory balance, if:
             #  - VM have responded to previous request (with some safety margin)
@@ -176,6 +178,7 @@ class SystemState:
     def mem_set(self, domid, val) -> None:
         self.log.info("mem-set domain {} to {}".format(domid, val))
         dom = self.dom_dict[domid]
+        assert not dom.paused
         dom.last_target = val
         # Can happen in the middle of domain shutdown apparently xc.lowlevel
         # throws exceptions too.
@@ -207,7 +210,8 @@ class SystemState:
         self.log.debug("inhibit_balloon_up()")
         for domid, dom in self.dom_dict.items():
             if (
-                dom.mem_actual is not None
+                not dom.paused
+                and dom.mem_actual is not None
                 and dom.mem_actual + 200 * 1024 < dom.last_target
             ):
                 self.log.info(
@@ -223,7 +227,8 @@ class SystemState:
         prev_mem_actual: dict[str, Optional[int]] = {}
 
         for dom in self.dom_dict.values():
-            dom.no_progress = False
+            if not dom.paused:
+                dom.no_progress = False
 
         #: helper array for holding free memory size, CHECK_PERIOD_S seconds
         #: ago, at every loop iteration
@@ -283,7 +288,8 @@ class SystemState:
         }
 
         for _, dom in dom_dict.items():
-            dom.no_progress = False
+            if not dom.paused:
+                dom.no_progress = False
 
         mem_set_threshold = 1.1
         succeeded = []
@@ -337,7 +343,9 @@ class SystemState:
                         memset_reqs[domid] = mem_pref
                         self.log.info("adjusted pref to '%s'", mem_pref)
                 diff = round(dom.mem_actual / memset_reqs[domid], 2)
-                if domid not in succeeded and diff <= mem_set_threshold:
+                if domid not in succeeded and (
+                    diff <= mem_set_threshold or dom.paused
+                ):
                     succeeded.append(domid)
                 self.log.debug(
                     "round '%d' dom '%s' has actual mem of %s (%sx)",
@@ -430,11 +438,12 @@ class SystemState:
             if dom.mem_used is not None:
                 self.log.info(
                     "stat: dom {!r} act={} pref={} last_target={}"
-                    "{}{}".format(
+                    "{}{}{}".format(
                         domid,
                         dom.mem_actual,
                         qubes.qmemman.algo.pref_mem(dom),
                         dom.last_target,
+                        " paused" if dom.paused else "",
                         " no_progress" if dom.no_progress else "",
                         (" slow_memset_react" if dom.slow_memset_react else ""),
                     )
